03b63465c98877acd097f2c7819497bf0ca3877d
Jack <namelockcaj@gmail.com>
2024-05-15 13:11:44 +0100

    fix: notfication counter updates with unread posts

--- notifications-page-main/scripts.js
+++ notifications-page-main/scripts.js
@@ -1,8 +1,12 @@
 const btnMarkAllRead = document.querySelector('.mark-all');
 const unreadPosts = document.querySelectorAll('.post.unread');
+const notificationCounter = document.querySelector('.notification-counter');
+notificationCounter.textContent = unreadPosts.length;
 
 btnMarkAllRead.addEventListener('click', () => {
     unreadPosts.forEach(post => {
         post.classList.remove('unread');
+        notificationCounter.textContent = '0'
     })
 })
+
